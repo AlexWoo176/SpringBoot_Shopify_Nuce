ed00b092970d421f7b827c7b6853363b77df222c
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-09-30 14:16:43 +0700

    add API Image

--- src/main/java/com/nextpay/vimo/controller/ImageController.java
+++ src/main/java/com/nextpay/vimo/controller/ImageController.java
@@ -1,4 +1,52 @@
 package com.nextpay.vimo.controller;
 
+import com.nextpay.vimo.model.Image;
+import com.nextpay.vimo.service.IImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/images")
 public class ImageController {
+    @Autowired
+    private IImageService imageService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Image>> getAllImage() {
+        return new ResponseEntity<>(imageService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Image> createNewImage(@RequestBody Image image) {
+        return new ResponseEntity<>(imageService.save(image), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Image> getImage(@PathVariable Long id) {
+        Optional<Image> imageOptional = imageService.findById(id);
+        return imageOptional.map(image -> new ResponseEntity<>(image, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Image> updateImage(@PathVariable Long id, @RequestBody Image image) {
+        Optional<Image> imageOptional = imageService.findById(id);
+        return imageOptional.map(image1 -> {
+            image.setId(image1.getId());
+            return new ResponseEntity<>(imageService.save(image), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Image> deleteImage(@PathVariable Long id) {
+        Optional<Image> imageOptional = imageService.findById(id);
+        return imageOptional.map(image -> {
+            imageService.remove(id);
+            return new ResponseEntity<>(image, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
