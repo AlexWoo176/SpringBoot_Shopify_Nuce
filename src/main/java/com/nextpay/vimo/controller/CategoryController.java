7b0bddb1933cc92d2b72122480eaf7d1129de7d9
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-09-30 14:08:44 +0700

    add API Category

--- src/main/java/com/nextpay/vimo/controller/CategoryController.java
+++ src/main/java/com/nextpay/vimo/controller/CategoryController.java
@@ -1,4 +1,65 @@
 package com.nextpay.vimo.controller;
 
+import com.nextpay.vimo.model.Category;
+import com.nextpay.vimo.model.Product;
+import com.nextpay.vimo.service.ICategoryService;
+import com.nextpay.vimo.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    private ICategoryService categoryService;
+
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Category>> getAllCategory() {
+        return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createNewCategory(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable Long id) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        return categoryOptional.map(category -> new ResponseEntity<>(category, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        return categoryOptional.map(category1 -> {
+            category.setId(category1.getId());
+            return new ResponseEntity<>(categoryService.save(category), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable Long id) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        return categoryOptional.map(category -> {
+            categoryService.remove(id);
+            return new ResponseEntity<>(category, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/{id}/products")
+    public ResponseEntity<Iterable<Product>> findAllProductByCategory(@PathVariable Long id) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        return categoryOptional.map(category -> new ResponseEntity<>(productService.findAllByCategory(category),
+                HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
